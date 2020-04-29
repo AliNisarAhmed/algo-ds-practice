28c4969caaf9244bd69c6949fb5e3b0f6de67206
Ali Ahmed <ali.ahmed@punchcard.io>
2020-04-29 01:13:54 -0600

    Solved Binary heap insert and extract max, find recursive solution to extractMax

---
+++ JS/DataStructures/Binary Heaps/maxBinaryheap.js
@@ -0,0 +1,80 @@
+class MaxBinaryHeap {
+	constructor() {
+		this.values = [];
+	}
+
+	insert(v) {
+		this.values.push(v);
+		let index = this.values.length - 1;
+		let parentIndex = Math.floor((index - 1) / 2);
+		let parent = this.values[parentIndex];
+		while (parent < v && parentIndex >= 0) {
+			this.values[parentIndex] = v;
+			this.values[index] = parent;
+			index = parentIndex;
+			parentIndex = Math.floor((parentIndex - 1) / 2);
+			parent = this.values[parentIndex];
+		}
+		return this;
+	}
+
+	// Also called remove
+	// return the largest value in the heap (which will be the root)
+	// extracting the largest value unbalances the heap, so we need to
+	// rearrange elements into a valid binary heap.
+	extractMax() {
+		const { values } = this;
+
+    // swap the first and the last
+    this.swap(0, this.values.length - 1);
+
+    // now the last is the one that we wanted to remove
+    const removed = values.pop();
+
+    // start the "sinking down" of the now root
+		let parentIndex = 0;
+		while (parentIndex < values.length) {
+			const v = values[parentIndex];
+			const leftChildIdx = 2 * parentIndex + 1;
+			const rightChildIdx = 2 * parentIndex + 2;
+			const left = values[leftChildIdx];
+      const right = values[rightChildIdx];
+
+      if (!left) {
+        if (right && right > v) {
+          this.swap(rightChildIdx, parentIndex);
+          parentIndex = rightChildIdx;
+        }
+        break;
+      } else if (!right) {
+        if (left && left > v) {
+          this.swap(leftChildIdx, parentIndex);
+          parentIndex = leftChildIdx;
+        }
+        break;
+      } else if (left > v || right > v) {
+				if (left >= right) {
+          this.swap(leftChildIdx, parentIndex);
+          parentIndex = leftChildIdx;
+				} else {
+          this.swap(rightChildIdx, parentIndex);
+          parentIndex = rightChildIdx;
+        }
+      }
+      break;
+    }
+
+    return removed;
+  }
+
+  swap(first, second) {
+    [ this.values[first], this.values[second] ] =
+      [ this.values[second], this.values[first] ];
+  }
+}
+
+const heap = new MaxBinaryHeap();
+heap.insert(100).insert(200).insert(150).insert(300).insert(400).insert(500);
+const max = heap.extractMax();
+console.log('max', max);
+console.log('heap', heap)
--- JS/DataStructures/BinarySearchTree/bst.js
+++ JS/DataStructures/BinarySearchTree/bst.js
@@ -1,107 +1,176 @@
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
 }
 
 class BST {
-  constructor() {
-    this.root = null;
-  }
+	constructor() {
+		this.root = null;
+	}
 
-  insert(value) {
-    let newNode = new Node(value);
-    if (!this.root) {
-      this.root = newNode;
-    } else {
-      let current = this.root;
-      while(true) {
-        if (value < current.value) {
-          if (!current.left) {
-            current.left = newNode;
-            return this;
-          } else {
-            current = current.left;
-          }
-        } else {
-          if (!current.right) {
-            current.right = newNode;
-            return this;
-          } else {
-            current = current.right;
-          }
-        }
-      }
-    }
-  }
+	insert(value) {
+		let newNode = new Node(value);
+		if (!this.root) {
+			this.root = newNode;
+			return this;
+		} else {
+			let current = this.root;
+			while (true) {
+				if (value < current.value) {
+					if (!current.left) {
+						current.left = newNode;
+						return this;
+					} else {
+						current = current.left;
+					}
+				} else {
+					if (!current.right) {
+						current.right = newNode;
+						return this;
+					} else {
+						current = current.right;
+					}
+				}
+			}
+		}
+	}
 
-  insert2(value) {
-    let newNode = new Node(value);
-    if (!this.root) {
-      this.root = newNode;
-      return this;
-    } else {
-      this.insertR(this.root, newNode);
-      return this;
-    }
-  }
+	insert2(value) {
+		let newNode = new Node(value);
+		if (!this.root) {
+			this.root = newNode;
+			return this;
+		} else {
+			this.insertR(this.root, newNode);
+			return this;
+		}
+	}
 
-  insertR(current, newNode) {
-    if (newNode.value < current.value) {
-      if (current.left === null) {
-        current.left = newNode;
-      } else {
-        this.insertR(current.left, newNode);
-      }
-    } else {
-      if (current.right === null) {
-        current.right = newNode;
-      } else {
-        this.insertR(current.right, newNode);
-      }
-    }
-  }
+	insertR(current, newNode) {
+		if (newNode.value < current.value) {
+			if (current.left === null) {
+				current.left = newNode;
+			} else {
+				this.insertR(current.left, newNode);
+			}
+		} else {
+			if (current.right === null) {
+				current.right = newNode;
+			} else {
+				this.insertR(current.right, newNode);
+			}
+		}
+	}
 
-  find(value) {
-    if (!this.root) {
-      return false;
-    }
-    return this.findR(this.root, value);
-  }
+	find(value) {
+		if (!this.root) {
+			return false;
+		}
+		return this.findR(this.root, value);
+	}
 
-  findR(current, v) {
-    if (current === null)
-      return false;
-    if (current.value === v)
-      return true;
-    if (v > current.value)
-      return this.findR(current.right, v);
+	findR(current, v) {
+		if (current === null) return false;
+		if (current.value === v) return true;
+		if (v > current.value) return this.findR(current.right, v);
 
-    return this.findR(current.left, v);
-  }
+		return this.findR(current.left, v);
+	}
 
-  // iterative
-  find2(value) {
-    if (!this.root)
-      return false;
-    let current = this.root;
-    while(true) {
-      if (!current)
-        return false;
-      if (current.value === value)
-        return true;
-      if (value > current.value)
-        current = current.right;
-      if (value < current.left)
-        current = current.left;
-    }
-  }
+	// iterative
+	find2(value) {
+		if (!this.root) return false;
+		let current = this.root;
+		while (true) {
+			if (!current) return false;
+			if (current.value === value) return true;
+			if (value > current.value) current = current.right;
+			if (value < current.left) current = current.left;
+		}
+	}
 }
 
 const bst = new BST();
-bst.insert2(10);
-bst.insert2(12);
+bst.insert(10).insert(15).insert(6).insert(20).insert(3).insert(8);
+
+//  ---- Tree Traversal ----
+
+function bfs(tree) {
+	const visited = [];
+	const queue = [];
+	queue.push(tree.root);
+	while (queue.length > 0) {
+		let current = queue.shift();
+		visited.push(current.value);
+		if (current.left) {
+			queue.push(current.left);
+		}
+		if (current.right) {
+			queue.push(current.right);
+		}
+	}
+	return visited;
+}
+
+console.log(bfs(bst));
+
+// Depth First Search
+// https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
+
+function dfs(tree) {
+	const result = {
+		preorder: [],
+		postorder: [],
+		inorder: [],
+	};
+	const current = tree.root;
+
+	preorder(current);
+	postorder(current);
+	inorder(current);
+
+	return result;
+
+	// Left, Root, Right
+	// All the lefts, then the roots, then the corresponding rights
+	function inorder(curr) {
+		if (curr.left) {
+			inorder(curr.left);
+		}
+		result.inorder.push(curr.value);
+		if (curr.right) {
+			inorder(curr.right);
+		}
+	}
+
+	// Root, Left, Right
+	// root, all the lefts, all the rights
+	function preorder(curr) {
+		result.preorder.push(curr.value);
+		if (curr.left) {
+			preorder(curr.left);
+		}
+		if (curr.right) {
+			preorder(curr.right);
+		}
+	}
+
+	// Left, Right, Root
+	// all the lefts, all the rights, then the root
+	function postorder(curr) {
+		if (curr.right) {
+			postorder(curr.right);
+		}
+		if (curr.left) {
+			postorder(curr.left);
+		}
+		result.postorder.push(curr.value);
+	}
+}
+
+console.log(dfs(bst));
 
 module.exports = BST;
