8430d1a9a85a94f4f4d581b03a17ef6ade5f329e
Ali Ahmed <ali.ahmed@punchcard.io>
2020-07-12 22:09:12 -0600

    Highest product of 3

--- CSharp/InterviewCake/GreedyAlgorithms/Greedy.cs
+++ CSharp/InterviewCake/GreedyAlgorithms/Greedy.cs
@@ -32,5 +32,65 @@ namespace CSharp.InterviewCake.GreedyAlgorithms
 
             return max;
         }
+    
+        
+        public static int HighestProductOf3(int[] inputs)
+        {
+            if (inputs.Length < 3)
+                throw new ArgumentException();
+
+            var result = inputs[0] * inputs[1] * inputs[2];
+            var highest2 = inputs[0] * inputs[1];
+            var lowest2 = inputs[0] * inputs[1];
+            var max = Math.Max(inputs[0], inputs[1]);
+            var min = Math.Min(inputs[0], inputs[1]);
+
+            for (var i = 2; i < inputs.Length; i++)
+            {
+                var current = inputs[i];
+
+                if (highest2 * current > result)
+                {
+                    result = highest2 * current;
+                }
+
+                if (lowest2 * current > result)
+                {
+                    result = lowest2 * current;
+                }
+
+                if (max * current > highest2)
+                {
+                    highest2 = max * current;
+                }
+
+                if (max * current < lowest2)
+                {
+                    lowest2 = max * current;
+                }
+
+                if (current > max)
+                {
+                    max = current;
+                }
+
+                if (min * current < lowest2)
+                {
+                    lowest2 = min * current;
+                }
+
+                if (min * current > highest2)
+                {
+                    highest2 = min * current;
+                }
+
+                if (current < min)
+                {
+                    min = current;
+                }
+            }
+
+            return result;
+        }
     }
 }
---
+++ Tests-CSharp/HighestProductOf3Tests.cs
@@ -0,0 +1,70 @@
+﻿using CSharp.InterviewCake.GreedyAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests_CSharp
+{
+    public class HighestProductOf3Tests
+    {
+        [Fact]
+        public void ShortArrayTest()
+        {
+            var actual = Greedy.HighestProductOf3(new int[] { 1, 2, 3, 4 });
+            var expected = 24;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LongerArrayTest()
+        {
+            var actual = Greedy.HighestProductOf3(new int[] { 6, 1, 3, 5, 7, 8, 2 });
+            var expected = 336;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ArrayHasOneNegativeTest()
+        {
+            var actual = Greedy.HighestProductOf3(new int[] { -5, 4, 8, 2, 3 });
+            var expected = 96;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ArrayHasTwoNegativesTest()
+        {
+            var actual = Greedy.HighestProductOf3(new int[] { -10, 1, 3, 2, -10 });
+            var expected = 300;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ArrayHasAllNegativesTest()
+        {
+            var actual = Greedy.HighestProductOf3(new int[] { -5, -1, -3, -2 });
+            var expected = -6;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ExceptionWithEmptyArrayTest()
+        {
+            Assert.Throws<ArgumentException>(() => Greedy.HighestProductOf3(new int[] { }));
+        }
+
+        [Fact]
+        public void ExceptionWithOneNumberTest()
+        {
+            Assert.Throws<ArgumentException>(() => Greedy.HighestProductOf3(new int[] { 1 }));
+        }
+
+        [Fact]
+        public void ExceptionWithTwoNumbersTest()
+        {
+            Assert.Throws<ArgumentException>(() => Greedy.HighestProductOf3(new int[] { 1, 1 }));
+        }
+
+    }
+}
