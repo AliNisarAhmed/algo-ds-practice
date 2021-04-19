fac88948db296d0eefd9e6ef1a4751ae5d7289bc
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-04-18 22:06:00 -0600

    dynamic programming

--- JS/LeetC/dynamicProgramming/houseRobber.js
+++ JS/LeetC/dynamicProgramming/houseRobber.js
@@ -1,44 +1,65 @@
-function rob(arr, memo = {}, start = 0, end = arr.length - 1) {
-	if (memo[start]) {
-		return memo[start];
-	}
-
-	if (end === start) {
-		memo[start] = arr[start];
-		return arr[start];
-	}
-
-	if (end - start === 1) {
-		memo[start] = Math.max(arr[start], arr[end]);
-		return memo[start];
-	}
-
-	let midpoint = Math.floor(start + (end - start) / 2);
-
-	let results = [];
-
-	for (let i = start; i <= midpoint; i++) {
-		if (i + 2 <= end) {
-			let r = rob(arr, memo, i + 2, end);
-			memo[start] = arr[i] + r;
-			results.push(memo[start]);
-		} else {
-			memo[start] = arr[i];
-			results.push(memo[start]);
-		}
-	}
-
-	return Math.max(...results);
+function rob(arr) {
+	const memo = {};
+	return helper(arr, memo, 0);
 }
 
-console.log(rob([2, 7, 9, 3, 1]));
+function helper(arr, memo, current) {
+	if (memo[current]) {
+		return memo[current];
+	}
 
-// function helper(arr, memo) {
-// 	if (arr.length === 1) {
-// 		return arr[0];
-// 	}
+	if (current >= arr.length) {
+		return 0;
+	}
 
-// 	if (arr.length === 2) {
-// 		return Math.max(arr[0], arr[1]);
-// 	}
-// }
+	if (current === arr.length - 1) {
+		memo[current] = arr[current];
+		return arr[current];
+	}
+
+	memo[current] = Math.max(
+		helper(arr, memo, current + 1),
+		helper(arr, memo, current + 2) + arr[current]
+	);
+
+	return memo[current];
+}
+
+console.log(rob([1, 2]));
+
+function rob(arr) {
+	if (arr.length === 0) {
+		return 0;
+	}
+
+	let maxRobbedAmount = [];
+	let n = arr.length;
+
+	maxRobbedAmount[n] = 0;
+	maxRobbedAmount[n - 1] = arr[n - 1];
+
+	for (let i = n - 2; i >= 0; i--) {
+		maxRobbedAmount[i] = Math.max(maxRobbedAmount[i + 1], maxRobbedAmount[i + 2] + arr[i]);
+	}
+
+	return maxRobbedAmount[0];
+}
+
+function rob(arr) {
+	if (arr.length === 0) {
+		return 0;
+	}
+
+	let n = arr.length;
+	let robNext = arr[n - 1];
+	let robNextPlusOne = 0;
+
+	for (let i = n - 2; i >= 0; i--) {
+		current = Math.max(robNext, robNextPlusOne + arr[i]);
+
+		robNextPlusOne = robNext;
+		robNext = current;
+	}
+
+	return robNext;
+}
--- JS/LeetC/dynamicProgramming/maxSubArray.js
+++ JS/LeetC/dynamicProgramming/maxSubArray.js
@@ -1,3 +1,26 @@
 function maxSubArray(nums) {
+	let maxSubArray = Number.NEGATIVE_INFINITY;
 
+	for (let i = 0; i < nums.length; i++) {
+		currentSubArray = 0;
+
+		for (let j = i; j < nums.length; j++) {
+			currentSubArray += nums[j];
+			maxSubArray = Math.max(maxSubArray, currentSubArray);
+		}
+	}
+
+	return maxSubArray;
+}
+
+function maxSubArray(nums) {
+	let currentSubArray = nums[0];
+	let maxSubArray = nums[0];
+
+	for (let i = 1; i < nums.length; i++) {
+		currentSubArray = Math.max(nums[i], currentSubArray + nums[i]);
+		maxSubArray = Math.max(currentSubArray, maxSubArray);
+	}
+
+	return maxSubArray;
 }
